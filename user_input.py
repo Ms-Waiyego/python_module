e1dff908825ee9b021e67cee6d452ed88b84b877
Brenda Waiyego <waiyegobrenda99@gmail.com>
2024-02-28 18:38:18 +0300

    completed assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name:")
+age= input("Enter your age:")
+location=input("Enter your current location:")
+print('Hello', name ,"your are " ,age ,"years old is and you live in" , location)
